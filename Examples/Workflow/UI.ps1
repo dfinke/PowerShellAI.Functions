31701a33df6a8af47d09a7969dcd3aa144426df0
dfinke <finked@hotmail.com>
2023-08-04 17:28:28 -0400

    initial commit

---
+++ Examples/Workflow/UI.ps1
@@ -0,0 +1,14 @@
+Add-Type -AssemblyName PresentationFramework
+
+[xml]$xaml = @"
+<Window
+    xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+    xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+    Title="GPT Function Call" Height="800" Width="900" WindowStartupLocation="CenterScreen">
+</Window>
+"@
+
+$reader = (New-Object System.Xml.XmlNodeReader $xaml)
+$window = [Windows.Markup.XamlReader]::Load($reader)
+
+$window.ShowDialog() | Out-Null
